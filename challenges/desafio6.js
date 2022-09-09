4edad6de3e2e57ace8b8afd0c70dbcdc45e6a95b
ThiagoZardo <thiago.artevisual@gmail.com>
2022-09-08 23:24:07 -0300

    Ajuste nos erros de lint Instalei as dependencias

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,4 +1,4 @@
 db.produtos.find(
-  { $and: [{ curtidas: { $gt:10 } }, { curtidas: { $lt: 85 } }] },
-  { _id: 0, nome: 1, curtidas: 1, },
+  { $and: [{ curtidas: { $gt: 10 } }, { curtidas: { $lt: 85 } }] },
+  { _id: 0, nome: 1, curtidas: 1 },
 );
