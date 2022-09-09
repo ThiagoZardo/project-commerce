240df985148f9c3db5d39d44de503a368a9e157f
ThiagoZardo <thiago.artevisual@gmail.com>
2022-09-09 00:43:30 -0300

    Requisito 14 ok

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,6 @@
 db.produtos.updateMany(
   { },
+  { valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } },
   { $set: { criadoPor: "Ronald McDonald" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
